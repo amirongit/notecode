ae58a25a6fe20d0629489debea975c0232a6afd9
amirhossein <pinoq@protonmail.com>
2021-08-24 15:25:29 +0430

    added comments and code untill repsonses section

--- werkzeug_sample_codes/quick_start.py
+++ werkzeug_sample_codes/quick_start.py
@@ -7,7 +7,7 @@ from werkzeug.wrappers import Request
 # the request.
 # It is passed to the WSGI application.
 # Environ dictionary has an item with the key 'wsgi.input' which is a BinaryIO
-# object to which represents a low level access to the form data.
+# object which represents a low level access to the form data.
 # Data in environ dictionary is encoded.
 sample_environ = create_environ()
 print(f'{sample_environ["PATH_INFO"]},'
@@ -25,16 +25,36 @@ print(f'{sample_request.path},'
       f' {sample_request.server},'
       f' {sample_request.method}')
 
-# url arguments and from data from put and post requests can be accessed
-# through Request objects.
-# Headers, url argumetns and form data can be accessed in a Request object
-# by using it's attributes.
+# Headers, url argumetns and form data from put and post requests can be
+# accessed through Request object's attributes.
 data = 'sample_post_key=sample+post+value'
 request_with_data = Request.from_values(
         query_string='sample_arguemtn=sample+value',
         content_length=len(data), input_stream=StringIO(data),
         content_type='application/x-www-form-urlencoded',
         method='POST')
-print(f'{request_with_data.args},\n'
-      f'{request_with_data.form},\n',
+print(f'{request_with_data.args}\n'
+      f'{request_with_data.form}\n'
       f'{request_with_data.headers}')
+
+
+typical_environ = create_environ()
+typical_environ.update(
+    HTTP_ACCEPT='text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q='
+    '0.8',
+    HTTP_ACCEPT_LANGUAGE='de-at,en-us;q=0.8,en;q=0.5',
+    HTTP_ACCEPT_ENCODING='gzip,deflate',
+    HTTP_ACCEPT_CHARSET='ISO-8859-1,utf-8;q=0.7,*;q=0.7',
+    HTTP_IF_MODIFIED_SINCE='Fri, 20 Feb 2009 10:10:25 GMT',
+    HTTP_IF_NONE_MATCH='"e51c9-1e5d-46356dc86c640"',
+    HTTP_CACHE_CONTROL='max-age=0')
+typical_request = Request(typical_environ)
+
+# With some HTTP headers the browser infroms web application what kinds of
+# mimtypes, languages, encodings and charsets it can accept in order of the
+# quality of handle.
+# These headers can be accessed through Request object's attributes.
+print(f'{typical_request.accept_mimetypes}\n'
+      f'{typical_request.accept_languages}\n'
+      f'{typical_request.accept_encodings}\n'
+      f'{typical_request.accept_charsets}')
