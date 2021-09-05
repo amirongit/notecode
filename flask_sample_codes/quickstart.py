e287a65d84ad21c7587c3aa408114804599866a7
amirhossein <pinoq@protonmail.com>
2021-09-05 19:52:04 +0430

    added comments and code until http methods

---
+++ flask_sample_codes/quickstart.py
@@ -0,0 +1,60 @@
+from markupsafe import escape
+
+from flask import Flask, url_for, request
+
+# An instance of Flask class will play the role of a WSGI application.
+# The first parameter of the constructor method of Flask class is the name of
+# the module which contains the application and used to look for resources.
+minimal_application = Flask(__name__)
+
+
+# Using the route decorator, the given url is bound to the function. so
+# The default content type used by a view function is HTML.
+@minimal_application.route('/')
+def index():
+    return '<h1>This is the index page!</h1>'
+
+
+# When returning html directly, any user provided input should be escaped in
+# order be secure from code injection.
+# Variable sections can be added to a url by putting variable names inside <>
+# In the given url to flask.
+# The view function receives the variable rules as keyword arguments.
+# A convertor can be used to specify the type of variable rules existing in
+# the url.
+# The default convertor used for url variables is string.
+@minimal_application.route('/greet/<string:name>/')
+def greet(name):
+    return f'<h1>Hello, {escape(name)}!</h1>'
+
+
+# If a url has a trailing slash, accessing the url without a trailing slash
+# causes flask to redirect the user to the original url.
+# If a url doesn't have a trailing slash, accessing the url with a trailing
+# slash results in a 404 erro.
+
+
+# In order to retriev the url of a veiw function, url_for function can be
+# used.
+# url_for function accepts the name of a view function as it's first argument
+# and accepts any number of keyword arguments to be used as variable rules.
+# If a given keyword argument to url_for function doesn't exist in the url of
+# the given view function, it will be appended to the returning url as query
+# arguments.
+# test_request_context is a method of Flask object which tells it to act like
+# it is handling a request.
+with minimal_application.test_request_context():
+    print(url_for('index'))
+    print(url_for('greet', name='AmirHossein',
+                  sample_unknown_key='sample value'))
+
+
+# By default, a view function only responses to GET requests.
+# In order to specify allowed HTTP methods for requests to be handled, their
+# name can be passed as a list to methods argument of the route method.
+@minimal_application.route('/postget/', methods=['POST', 'GET'])
+def postget():
+    if request.method == 'POST':
+        ...
+    if request.method == 'GET':
+        ...
