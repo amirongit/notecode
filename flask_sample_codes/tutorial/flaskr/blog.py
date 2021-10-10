f02530e459812b8ae8340a7a69d9f3fe0a8a40d8
amirhossein <pinoq@protonmail.com>
2021-10-10 12:42:07 +0330

    fixed project code

--- flask_sample_codes/tutorial/flaskr/blog.py
+++ flask_sample_codes/tutorial/flaskr/blog.py
@@ -37,7 +37,7 @@ def create():
             db.commit()
             return redirect(url_for('blog.index'))
 
-        return render_template('blog/create.html')
+    return render_template('blog/create.html')
 
 
 def get_post(id, check_author=True):
