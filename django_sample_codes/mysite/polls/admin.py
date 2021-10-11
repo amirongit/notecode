e0c18fb84ab29cde24fdc5fb9d8b57fe5a598f93
amirhossein <pinoq@protonmail.com>
2021-10-11 18:14:54 +0330

    added polls models to admin site

--- django_sample_codes/mysite/polls/admin.py
+++ django_sample_codes/mysite/polls/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Question
+
+# In order to register a model as an interface to django admingg app,
+# adming.site.register function can be used.
+admin.site.register(Question)
