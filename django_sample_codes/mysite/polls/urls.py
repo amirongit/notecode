33dcfc42bafac833c690bd7d4805ea77e39d5b7f
amirhossein <pinoq@protonmail.com>
2021-10-13 19:58:01 +0330

    added code and comments until index template

--- django_sample_codes/mysite/polls/urls.py
+++ django_sample_codes/mysite/polls/urls.py
@@ -4,6 +4,12 @@ from . import views
 
 # In order for a view function to be called, it should be mapped to a url in
 # URLconf.
+# In order to capture a part of the url as a kwarg in a view, angle brackets
+# can be used in it's associated urlpattern.the expression in angle brackets
+# should be like <convertor:var_name>.
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote')
 ]
--- django_sample_codes/mysite/polls/views.py
+++ django_sample_codes/mysite/polls/views.py
@@ -1,8 +1,25 @@
 from django.http import HttpResponse
 
+from .models import Question
+
 # Create your views here.
 
 
 # In order to create a basic http response, HttpResponse class can be used.
 def index(request):
-    return HttpResponse('Hello, world! Your at the polls index.')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    return HttpResponse(
+        ', '.join([q.question_text for q in latest_question_list]))
+
+
+def detail(request, question_id):
+    return HttpResponse(f'You\'re looking at question number {question_id}')
+
+
+def results(request, question_id):
+    response = f'You\'r looking at the results of question {question_id}'
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    return HttpResponse(f'You\'re voting on question {question_id}')
