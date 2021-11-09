555cc0b235fadce5f1328f4a6a18bfa468315b2d
amirhossein <pinoq@protonmail.com>
2021-11-09 16:29:53 +0330

    added code and comments until extracting data section from scrapy docs

--- scrapy_sample_codes/tutorial/tutorial/spiders/quoutes_spider.py
+++ scrapy_sample_codes/tutorial/tutorial/spiders/quoutes_spider.py
@@ -7,15 +7,27 @@ class QuotesSpider(scrapy.Spider):
     # identifies the spider and must be unique within a project
     name = 'quotes'
 
-    # should return an iterable of Request objects which the spider will begin
-    # to crawl from
-    def start_requests(self):
+    # start_requests method should return an iterable of Request objects which
+    # the spider will begin to crawl from
+    # scrapy will schedule these Request objects and for each of their
+    # Response objects the callback method will be called given the object
+    def start_requests_(self):
         urls = [
             'http://quotes.toscrape.com/page/1/',
             'http://quotes.toscrape.com/page/2/',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
+
+    # start_requests method also has a default implementation which uses
+    # start_urls class attribute to generate the initial iterable of Request
+    # objects the class attribute could be a list of urls
+    # the default callback method for the generated Request objects will be
+    # parse
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+        'http://quotes.toscrape.com/page/2/',
+    ]
 
     # the method which is called to handle the response of each request made
     # always passed a TextResponse object
