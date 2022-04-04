f5ea5e565319511e469f986c669eda69151f4418
AmirHossein Ahmadi <amirthehossein@gmail.com>
2022-04-04 11:28:12 +0430

    satisfied linter with drf tutorial project

--- django_rest_framework/tutorial/snippets/urls.py
+++ django_rest_framework/tutorial/snippets/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from rest_framework import renderers
 from rest_framework.routers import DefaultRouter
-from rest_framework.urlpatterns import format_suffix_patterns
 
 from snippets import views
 
@@ -57,8 +56,8 @@ explicit_urlpatterns = [
 # handled automatically. the DefaultRouter also creates the root view.
 
 router = DefaultRouter()
-router.register(r'snippets', views.SnippetViewSet,basename="snippet")
-router.register(r'users', views.UserViewSet,basename="user")
+router.register(r'snippets', views.SnippetViewSet, basename="snippet")
+router.register(r'users', views.UserViewSet, basename="user")
 
 urlpatterns = [
     path('', include(router.urls))
