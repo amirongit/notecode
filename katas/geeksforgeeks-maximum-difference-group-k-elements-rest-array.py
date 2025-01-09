b88908d5c42b5989667b4e50ce0885911c8127c3
AmirHossein Ahmadi <amirthehossein@gmail.com>
2025-01-09 14:01:14 +0330

    add solution for a gfg kata

---
+++ katas/geeksforgeeks-maximum-difference-group-k-elements-rest-array.py
@@ -0,0 +1,6 @@
+def gigaChad(arr: list[int], k: int) -> int:
+    sorted_arr = sorted(arr)
+    return max(
+        abs(sum(sorted_arr[:k]) - sum(sorted_arr[k:])),
+        abs(sum(sorted_arr[:-k]) - sum(sorted_arr[k + 1:]))
+    )
