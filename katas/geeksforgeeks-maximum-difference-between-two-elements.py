56a274fa987901b4ff89d20e502838571d47ede8
AmirHossein Ahmadi <amirthehossein@gmail.com>
2025-01-10 19:34:11 +0330

    refine gfg kata solution

--- katas/geeksforgeeks-maximum-difference-between-two-elements.py
+++ katas/geeksforgeeks-maximum-difference-between-two-elements.py
@@ -1,21 +1,7 @@
-def low_iq(arr: list[int]) -> int:
-    max_diff: int = 0
-    for fidx, fnum in enumerate(arr):
-        for snum in arr[fidx + 1:]:
-            max_diff = max(snum - fnum, max_diff)
-
-    return max_diff
-
-
-def maxDiff(arr: list[int]) -> int:
-    if len(arr) < 2:
-        return 0
-
+def max_diff_between_two_elements(arr: list[int]) -> int:
     min_val = arr[0]
-    max_diff = 0
-
-    for num in arr[1:]:
-        max_diff = max(max_diff, num - min_val)
-        min_val = min(min_val, num)
-
-    return max_diff
+    max_dif = 0
+    for number in arr:
+        max_dif = max(max_dif, number - min_val)
+        min_val = min(min_val, number)
+    return max_dif
