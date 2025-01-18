c0897a23bdd9e1091b068c8333a2bd1d3a11c288
AmirHossein Ahmadi <amirthehossein@gmail.com>
2025-01-18 21:29:23 +0330

    remove main condition

--- katas/codewars-537e18b6147aa838f600001b.py
+++ katas/codewars-537e18b6147aa838f600001b.py
@@ -78,7 +78,3 @@ class LineInfo(NamedTuple):
     start_word_index: int
     end_word_index: int
     extra_spaces: int = 0
-
-
-if __name__ == '__main__':
-    justify('''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sagittis dolor mauris, at elementum ligula tempor eget. In quis rhoncus nunc, at aliquet orci. Fusce at dolor sit amet felis suscipit tristique. Nam a imperdiet tellus. Nulla eu vestibulum urna. Vivamus tincidunt suscipit enim, nec ultrices nisi volutpat ac. Maecenas sit amet lacinia arcu, non dictum justo. Donec sed quam vel risus faucibus euismod. Suspendisse rhoncus rhoncus felis at fermentum. Donec lorem magna, ultricies a nunc sit amet, blandit fringilla nunc. In vestibulum velit ac felis rhoncus pellentesque. Mauris at tellus enim. Aliquam eleifend tempus dapibus. Pellentesque commodo, nisi sit amet hendrerit fringilla, ante odio porta lacus, ut elementum justo nulla et dolor.''', 759)
